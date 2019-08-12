ab78622579afe63c1a3540bce613ff05b9786f8a
Kevin Delgado <kevin.delgado@globant.com>
2019-08-12 11:18:15 -0400

    Added MembersScene

---
+++ Clean-iOS/Presentation/MembersScene/MembersContracts.swift
@@ -0,0 +1,15 @@
+//
+//  MembersContracts.swift
+//  Clean-iOS
+//
+//  Created by Kevin Ignacio Delgado Jimenez on 8/12/19.
+//  Copyright © 2019 Kevin Ignacio Delgado Jimenez. All rights reserved.
+//
+
+protocol MembersViewContract {
+    
+}
+
+protocol MembersPresenterContract {
+    
+}
---
+++ Clean-iOS/Presentation/MembersScene/MembersPresenter.swift
@@ -0,0 +1,11 @@
+//
+//  MembersPresenter.swift
+//  Clean-iOS
+//
+//  Created by Kevin Ignacio Delgado Jimenez on 8/12/19.
+//  Copyright © 2019 Kevin Ignacio Delgado Jimenez. All rights reserved.
+//
+
+class MembersPresenter {
+    
+}
